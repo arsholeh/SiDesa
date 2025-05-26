b802f433ab8100968d3e383101ec5dab2b64b43f
abdul rahman sholeh <focalornet11@gmail.com>
2025-05-26 15:03:36 +0700

    Menambahkan halaman dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
+});
+
+Route::get('/dashboard', function () {
+    return view('pages.dashboard');
 });
