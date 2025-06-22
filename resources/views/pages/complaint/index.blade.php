867fd6f8b3440182af5a0076251b23fb99ead706
abdul rahman sholeh <focalornet11@gmail.com>
2025-06-23 06:05:27 +0700

    Menambahkan halaman complaint

---
+++ resources/views/pages/complaint/index.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <!-- Page Heading -->
+  <div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Aduan</h1>
+    <a href="/complaint/create" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+      class="fas fa-plus fa-sm text-white-50"></i> Buat Aduan</a>
+  </div> 
+
+  <div class="row">
+    <div class="col">
+      <div class="card shadow">
+        <div class="card-body">
+          <table class="table tabe-bordered table-responsive">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Judul</th>
+                <th>Isi Aduan</th>
+                <th>Status</th>
+                <th>Foto Bukti</th>
+                <th>Tanggal Laporan</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            @if (count($complaints) < 1)
+              <tbody>
+                <tr>
+                  <td colspan="11">
+                    <p class="pt-3 text-center">Tidak ada data</p>
+                  </td>
+                </tr>
+              </tbody>
+              @else
+              <tbody>
+                @foreach ($complaints as $item)
+                <tr>
+                  <td>{{$loop->iteration + $complaints->firstItem() - 1}}</td>
+                  <td>{{ $item->title }}</td>
+                  <td>{!! wordwrap($item->content, 40, "<br>\n") !!}</td>
+                  <td>{{ $item->status_label }}</td>
+                  <td>
+                    @if (isset($item->photo_proof))
+                      <img src="{{ $item->photo_proof }}" alt="Foto Bukti" style="max-width: 300px;">
+                    @else
+                      Tidak Ada  
+                    @endif
+                  </td>
+                  <td>{{ $item->report_date_label }}</td>
+                  <td>
+                    <div class="d-flex align-items-center" style="gap:10px;">
+                      <a href="/complaint/{{ $item->id }}" class="btn btn-sm btn-warning d-inline-block">
+                      <i class="fas fa-pen"></i>
+                      </a>
+                      <button href="/complaint/{{ $item->id }}" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#ConfirmationDelete-{{ $item->id }}">
+                        <i class="fas fa-eraser"></i>
+                        </button>                        
+                    </div>
+                    
+                  </td>
+                </tr>
+                @include('pages.complaint.confirmation-delete')
+                @endforeach
+              </tbody>
+            @endif
+            
+          </table>
+        </div>
+        @if ($complaints->lastPage() > 1 )
+          <div class="card-footer">
+          {{ $complaints->links('pagination::bootstrap-5') }}
+        </div>
+        @endif
+      </div>
+    </div>
+  </div>
+@endsection
